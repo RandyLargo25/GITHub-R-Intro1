7a79bbdfab3ceb297e6df5d39fb66b097439db44
Randy Largo <randylargo.cs3500@gmail.com>
2025-10-17 09:45:54 -0600

    B code

---
+++ setup.R
@@ -0,0 +1,30 @@
+# package load function
+packageLoad <-
+  function(x) {
+    for (i in 1:length(x)) {
+      if (!x[i] %in% installed.packages()) {
+        install.packages(x[i])
+      }
+      library(x[i], character.only = TRUE)
+    }
+  }
+
+# use function to set up environment
+packages <- c("tidyverse", 
+              "palmerpenguins", 
+              "rmarkdown",
+              "ggthemes",
+              "RColorBrewer",
+              "viridis",
+              "ggdark",
+              "plotly",
+              "lterdatasampler",
+              "rstatix",
+              "httr",
+              "jsonlite",
+              "dataRetrieval",
+              "usethis",
+              "gitcreds")
+
+packageLoad(packages)
+
